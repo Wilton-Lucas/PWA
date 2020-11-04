34df64afceaaf66e5c0a5ba50ca55a85b8d68d76
Buu - Monteiro <mcmonteiro97@gmail.com>
2020-11-03 23:41:25 -0300

    css e layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import  api from  './API';
 
@@ -11,118 +11,115 @@ import {Button} from 'primereact/button';
 import { DataScroller } from 'primereact/datascroller';
 
 
-class App extends React.Component {
-  constructor(props){
-    super(props);    
+class App extends Component {
+    constructor(props){
+        super(props);
 
-    this.state = {      
-      financas: [
-        
-          {
-            "id": "1",
-            "descricao": "conta de luz",
-            "dataVencimento": "20/10/2020",
-            "tipoReceita": 1,
-            "valor": "19.85",
-            "parcelasQtd": 1
-          },
-          {
-            "id": "2",
-            "descricao": "Caiu salário",
-            "dataVencimentoInicial": "20/10/2020",
-            "dataVencimentoFinal": "20/10/2020",
-            "tipoReceita": 2,
-            "valor": "2000.0",
-            "parcelasQtd": 1
-          },
-          {
-            "id": "3",
-            "descricao": "Fatura Nubank",
-            "dataVencimentoInicial": "20/10/2020",
-            "dataVencimentoFinal": "20/1/2021",
-            "tipoReceita": 1,
-            "valor": "6000.0",
-            "parcelasQtd": 3
-          }
+        this.state = {
+            financas: [],
+            mes: 'Janeiro'
+        };
+
+        this.itemTemplate = this.itemTemplate.bind(this);
+        this.mudarMes = this.mudarMes.bind(this);
+
+        console.log('finanças antes =' + this.itemTemplate);
+
+    }
 
 
-      ],
+    componentDidMount(){
+        api.get('').then((resposta)=>{
+
+            this.setState({financas:resposta.data});
+
+        });
+
+
+    }
+
+    itemTemplate(data) {
+        console.log('entrei kkkk')
+        return (
+            <div className="item">
+                {console.log(data)}
+                <div className="nome-item">{data.descricao}</div>
+                <div className="dt-vencimento">{data.dataVencimentoFinal}</div>
+                <div className="valor">{data.valor}</div>
+            </div>
+        );
     };
 
-    console.log('finanças antes =' + this.state.financas);
+    mudarMes() {
+        this.setState({mes:this.state.mes +1});
+        const response = api.get('');
+        console.log(response.data)
+        this.setState({financas: response.data});
 
-  }
+
+    };
 
 
-  componentDidMount(){        
+    render() {
+        return(
+            <div className="App">
 
-  
-   api.get('').then((resposta)=>{
-        
-        this.setState({financas:resposta.data});
-         console.log(this.state.financas[0].descricao);
+                <div className="App-header">
+                    <h2>Finanças</h2>
+                    <Button label="+" />
 
-   });
-   console.log('finanças dps1 =' + this.state.financas);
-  }
+                </div>
 
-  itemTemplate = (data)=> {
-    return (         
-        <div className="item">   
-            
-          <div className="nome-item">{data.descricao}</div>
-          <div className="dt-vencimento">{data.dataVencimentoFinal}</div>
-          <div className="valor">{data.valor}</div>                  
-        </div>
-    );
-  };
+                <div className="App-month">
+                    <ul className="nav">
+                        <li><Button label="<"/></li>
+                        <li><h3> {this.state.mes}  </h3></li>
+                        <li><Button label=">" onClick={this.mudarMes}/></li>
+                    </ul>
 
-  mudarMes = ()=> {      
-    this.setState({mes:this.state.mes +1});  
-    const response = api.get('');  
-    console.log(response.data)    
-    this.setState({financas: response.data});  
-    
+                </div>
 
-  };
-  
+                <div className="App-content">
+                    <DataScroller value={this.state.financas} itemTemplate={this.itemTemplate} rows={5} inline scrollHeight="500px" />
+                </div>
 
-  render() {
-    return(
-        <div className="App">
-          
-          <div className="App-header">
-              <h2>Finanças{this.state.financas[0].descricao}</h2>
-              <Button label="+" />
-              
-          </div>
-          
-          <div className="App-month">
-              <ul className="nav">
-                  <li><Button label="<"/></li>
-                  <li><h3> {this.state.mes}  </h3></li>
-                    <li><Button label=">" onClick={this.mudarMes}/></li>
-              </ul>
+                <div className="App-footer">
+                    <div className="App-footer-row">
+                        <div className="App-footer-label">
+                            <div>Receitas</div>
+                            <div>Despezas</div>
+                                <div>Saldo</div>
+                        </div>
+                        <div className="App-footer-content">
+                             <div>R$ 3000.00</div>
+                            <div>R$ 3000.00</div>
+                            <div>R$ 3000.00</div>
+                        </div>
+                    </div>
+                    {/*<div className="App-footer-row">*/}
+                    {/*    <div className="App-footer-label">*/}
+                    {/*        Despezas*/}
+                    {/*    </div>*/}
+                    {/*    <div className="App-footer-content">*/}
+                    {/*        R$ 3000.00*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    {/*<div className="App-footer-row">*/}
+                    {/*    <div className="App-footer-label">*/}
+                    {/*        Saldo*/}
+                    {/*    </div>*/}
+                    {/*    <div className="App-footer-content">*/}
+                    {/*        R$ 3000.00*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                </div>
 
-          </div>
-          
-          <div className="App-content">
-              <DataScroller value={this.state.financas} itemTemplate={this.itemTemplate} rows={5} inline scrollHeight="500px" />
-          </div>
 
-          <div className="App-footer">
-          Receitas - R$ 3000.00
-          Despezas - R$ 3000.00
-          Saldo  -   R$ 3000.00
-          
-          </div>
 
-         
 
-          
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
